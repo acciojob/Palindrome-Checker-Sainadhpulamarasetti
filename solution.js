ab5228df9723b94a48b615bac25a66f30c07ef4b
Sainadhpulamarasetti <35665123+Sainadhpulamarasetti@users.noreply.github.com>
2022-10-30 23:30:57 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,10 @@
 // complete the given function
 
 function palindrome(str){
-
+  let result = str.split("").reverse().join("")
+    if(result === str){
+        return true
+    }
+    return false
 }
 module.exports = palindrome
